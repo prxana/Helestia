eb8aeac5565fb4528b13d64ea7c677287a5552dc
Helena Pereira <165259887+prxana@users.noreply.github.com>
2026-01-16 11:52:27 +0100

    Update vite.config.ts

--- Ecommerce Site Design/vite.config.ts
+++ Ecommerce Site Design/vite.config.ts
@@ -2,6 +2,13 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/helestia/',
+})
 
   export default defineConfig({
     plugins: [react()],
